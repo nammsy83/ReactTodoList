54facb8abf3a550906e1a54e4261eb2615ba310a
Namrata Subhedar <nammsy@gmail.com>
2019-08-16 23:03:43 +0530

    Updates

---
+++ precache-manifest.a4c1c36ca68d40060b576181014999d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0545e8abfb9dc0e12b2e57a20de0f76b",
+    "url": "/ReactTodoList/index.html"
+  },
+  {
+    "revision": "c1af537f96abcfc4fc3b",
+    "url": "/ReactTodoList/static/css/main.6899ec9b.chunk.css"
+  },
+  {
+    "revision": "1f276c270abe2580869b",
+    "url": "/ReactTodoList/static/js/2.b0189219.chunk.js"
+  },
+  {
+    "revision": "c1af537f96abcfc4fc3b",
+    "url": "/ReactTodoList/static/js/main.5b901ce1.chunk.js"
+  },
+  {
+    "revision": "3ac544556a5a9ce0de87",
+    "url": "/ReactTodoList/static/js/runtime~main.954c082c.js"
+  }
+]);
